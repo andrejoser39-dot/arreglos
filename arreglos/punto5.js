3b74d6b4285b12998b690f437e66035b1e19a0e6
andrejoser39.dot <andrejoser39@gmail.com>
2026-03-27 17:57:13 -0500

    agregamos las actividades usando pull y push

---
+++ arreglos/punto5.js
@@ -0,0 +1,11 @@
+// 1. Crear un arreglo vacío
+let tareas = [];
+
+// 2. Agregar tres actividades utilizando push()
+tareas.push("Estudiar JavaScript");
+tareas.push("Hacer ejercicio");
+tareas.push("Lavar la ropa");
+
+// 3. Eliminar la última actividad utilizando pop()
+// (En este caso, se eliminará "Lavar la ropa")
+tareas.pop();

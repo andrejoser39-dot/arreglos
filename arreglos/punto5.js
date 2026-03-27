2206a7acd2a58a84f6dada93450bf41bd24aa0e0
andrejoser39.dot <andrejoser39@gmail.com>
2026-03-27 17:57:58 -0500

    agregamos las salidas de mensaje para terminar el 5to punto

--- arreglos/punto5.js
+++ arreglos/punto5.js
@@ -9,3 +9,9 @@ tareas.push("Lavar la ropa");
 // 3. Eliminar la última actividad utilizando pop()
 // (En este caso, se eliminará "Lavar la ropa")
 tareas.pop();
+// 4. Mostrar el resultado final
+console.log("Mi lista de tareas pendientes:");
+console.log(tareas);
+
+// Opcional: Mostrar cuántas tareas quedaron
+console.log("Total de tareas actuales:", tareas.length);

9ce972c96a2250c9489fd0f2f0a7837e47267528
andrejoser39.dot <andrejoser39@gmail.com>
2026-03-27 17:50:12 -0500

    empezamos con el ejercisio 3 de tranferencia

---
+++ arreglos/punto3.js
@@ -0,0 +1,9 @@
+//3. Notas de actividades: Cree un arreglo con seis notas numéricas. Recorra el arreglo  usando for y calcule el promedio (no use funciones externas). 
+// 1. Crear el arreglo con seis notas numéricas
+let notas = [4.5, 3.8, 5.0, 2.5, 4.2, 3.0];
+
+// 2. Variable para acumular la suma de las notas
+let sumaTotal = 0;
+
+// 3. Recorrer el arreglo usando un bucle for clásico
+for (let i = 0; i <

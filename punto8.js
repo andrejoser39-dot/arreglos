51ff4244f0151ea53f6850128bd54a7ffebd906e
brianbayona <brianmauriciobayonaravelo@gmail.com>
2026-03-27 18:03:58 -0500

    puntos del 6 al 10 terminados hecho a mano

---
+++ punto8.js
@@ -0,0 +1,8 @@
+let libro = {
+  titulo: "la vida de joser",
+  autor: "Brian Bayona",
+  num_pags: 12,
+  genero: "literario",
+};
+
+console.log("El libro '" + libro.titulo + "' del autor " + libro.autor + " pertenece al genero " + libro.genero + ".");

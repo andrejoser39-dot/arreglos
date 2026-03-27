7981b2b190c3cb9d6ec2bd2a26406544a6c1f4b6
andrejoser39.dot <andrejoser39@gmail.com>
2026-03-27 17:54:58 -0500

    terminamos el punto 4 del ejercisio comentando el codigo

---
+++ arreglos/punto4.js
@@ -0,0 +1,14 @@
+// 1. Crear el objeto con sus propiedades (pares de clave: valor)
+const celular = {
+  marca: "Samsung",
+  modelo: "Galaxy S21",
+  año: 2021,
+  estado: "Nuevo"
+};
+
+// 2. Mostrar cada propiedad de manera individual
+// Usaremos la "Notación de punto", que es la más común en JS
+console.log("Marca del equipo:", celular.marca);
+console.log("Modelo del equipo:", celular.modelo);
+console.log("Año de lanzamiento:", celular.año);
+console.log("Estado actual:", celular.estado);
